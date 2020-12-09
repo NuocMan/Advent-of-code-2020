d1c01454de7c282c05e16680672a7548ebf73f37
Valentin Grimaldi <valentin.grimaldi@outlook.com>
2020-12-09 16:18:04 +0100

    add day 3

---
+++ day3/ex1.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+if [[ $# != 1 || ! -f $1 ]] ; then
+    exit 1
+fi
+
+readarray -t LINE_ARRAY < $1
+map_width=$(echo -n ${LINE_ARRAY[0]} | wc -c)
+
+function tree_encountered() {
+    local counter=0
+    local line=0
+    local column=0
+
+    while (( line < ${#LINE_ARRAY[@]} )); do
+	if [[ ${LINE_ARRAY[line]:column:1} == '#' ]]; then
+	    (( counter++ ))
+	fi
+
+	column=$(( (column + $1) % map_width ))
+        line=$(( line + $2 ))
+    done
+    echo $counter
+}
+
+tree_encountered 3 1
