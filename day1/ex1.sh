89fe9b7f9a5bbd315c430300c9ccde5dda7435d3
Valentin Grimaldi <valentin.grimaldi@outlook.com>
2020-12-09 16:17:42 +0100

    update day 1

--- day1/ex1.sh
+++ day1/ex1.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
-if [[ $# != 1 || ! -f $1 ]]; then
+if [[ $# != 1 || ! -f $1 ]] ; then
     exit 1
 fi
 
 readarray -t LINE_ARRAY < $1
 
 for i in "${!LINE_ARRAY[@]}"; do
-    for j in "${LINE_ARRAY[@]:$i}"; do
+    for j in "${LINE_ARRAY[@]:$i+1}"; do
 	if (( LINE_ARRAY[i] + j == 2020 )); then
     	    echo $(( LINE_ARRAY[i] * j ))
 	    exit 0
--- day1/ex2.sh
+++ day1/ex2.sh
@@ -8,11 +8,11 @@ readarray -t LINE_ARRAY < $1
 
 for i in "${!LINE_ARRAY[@]}"; do
     j_indexes=( "${!LINE_ARRAY[@]}" )
-    for j in "${j_indexes[@]:$i}"; do
+    for j in "${j_indexes[@]:$i+1}"; do
 	if (( LINE_ARRAY[i] + LINE_ARRAY[j] >= 2020 )); then
 	    continue
 	fi
-	for k in "${LINE_ARRAY[@]:$j}"; do
+	for k in "${LINE_ARRAY[@]:$j+1}"; do
 	    if (( LINE_ARRAY[i] + LINE_ARRAY[j] + k == 2020 )); then
     		echo "${LINE_ARRAY[i]} * ${LINE_ARRAY[j]} * $k" = $(( LINE_ARRAY[i] * LINE_ARRAY[j] * k ))
 		exit 0
